d43963bcfc9b44f9078528b05041a02d27420a1c
Hisoka <borisenko.na.1001h@gmail.com>
2022-05-30 18:22:51 +0300

    Немного поменял

--- src/test/java/test/AllSuiteTest.java
+++ src/test/java/test/AllSuiteTest.java
@@ -5,16 +5,17 @@ import org.junit.platform.suite.api.Suite;
 
 import test.language.LanguageTest;
 import test.page.group.TestAddGroup;
-import test.page.idea.TestAddNewEmotionToTheWall;
 import test.page.login.TestLoginPage;
 import test.page.message.TestSendMessageUser;
 import test.page.message.TestSendParameterizedMessage;
 import test.page.music.TestAddMusic;
+import test.page.music.TestDeleteMusic;
 import test.page.photo.TestAddPhoto;
 import test.page.post.TestPost;
+import test.page.user.TestNameChange;
 
 @Suite
-@SelectClasses({TestPost.class, TestAddNewEmotionToTheWall.class, TestLoginPage.class, TestSendMessageUser.class,
-        TestSendParameterizedMessage.class, TestAddMusic.class, TestAddPhoto.class, TestAddGroup.class, LanguageTest.class})
+@SelectClasses({TestNameChange.class, TestPost.class, TestLoginPage.class, TestSendMessageUser.class, TestSendParameterizedMessage.class
+        , TestAddMusic.class, TestDeleteMusic.class, TestAddPhoto.class, TestAddGroup.class, LanguageTest.class})
 public class AllSuiteTest {
 }
